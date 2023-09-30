1c91f111b9ccde32c501ae6eef06ceca1af37d24
ilyag26 <gayvoronskiy.i02@gmail.com>
2023-09-30 17:35:45 +0200

    updated logic of game

--- basic/logic.go
+++ basic/logic.go
@@ -2,7 +2,17 @@ package basic
 
 import (
 	"fmt"
+	"slices"
 	"strings"
+)
+
+var (
+	letter            string
+	idLetter          int
+	checkGame         bool
+	iterationGameLose int
+	iterationGameWin  int
+	attemps           int
 )
 
 func PrintRules() {
@@ -11,26 +21,64 @@ func PrintRules() {
 		"\nWrite word that you think hidden and try to win!",
 		"\n-----------")
 }
+func gallowsPrint(attemps int) {
+	switch {
+	case attemps == 5:
+		fmt.Print("|--------\n|       |\n|       O\n|\n|\n|\n")
+	case attemps == 4:
+		fmt.Print("|--------\n|       |\n|       O\n|       |\n|\n|\n")
+	case attemps == 3:
+		fmt.Print("|--------\n|       |\n|       O\n|      /|\\\n|\n|\n")
+	case attemps == 2:
+		fmt.Print("|--------\n|       |\n|       O\n|      /|\\\n|	|\n|\n")
+	case attemps == 1:
+		fmt.Print("|--------\n|       |\n|       O\n|      /|\\\n|	|\n|      / \\\n")
+	}
+}
+func maskingWord(lengthWord int) []string {
+	var mask []string
+	for i := 0; i != lengthWord; i++ {
+		mask = append(mask, "*")
+	}
+	return mask
+}
 
 func TrimWord(words []string, randNumber int) []string {
 	return strings.Split(words[randNumber], "")
 }
 
-func PrintWordByStep(hiddenWord []string, letterArray []string, word string, numberRand int) {
-	var wordClue string
-	for i := 0; i != len(letterArray); i++ {
-		wordClue += letterArray[i]
-		fmt.Println("Try to guess te word -> ", "Following words: '",
-			wordClue, "' |", "Number of letters: ", len(letterArray))
-		fmt.Scan(&word)
-		if word == hiddenWord[numberRand] {
-			fmt.Println("DONE! -> YOU WIN!")
-			break
-		} else if wordClue == hiddenWord[numberRand] {
-			fmt.Println("You lose!")
-			break
+func PrintWordByStep(hiddenWord []string, wordsExplanation []string, letterArray []string, numberRand int) {
+	maskedWord := maskingWord(len(letterArray))
+	attemps = 6
+	fmt.Println("What is this? -> ", wordsExplanation[numberRand])
+	for !checkGame {
+		fmt.Println("Try to guess the word -> ", "Number of letters in this word: ", len(letterArray), "Remainings Attemps: ", attemps)
+		fmt.Print("Word is: ")
+		for _, value := range maskedWord {
+			fmt.Printf("%s", value)
+		}
+		fmt.Print("\nEnter letter: ")
+		fmt.Scan(&letter)
+		if slices.Contains(letterArray, letter) {
+			if iterationGameWin != len(letterArray)-1 {
+				idLetter = strings.Index(hiddenWord[numberRand], letter)
+				maskedWord[idLetter] = letter
+				iterationGameWin++
+				fmt.Printf("good, there is '%s'\n", letter)
+			} else {
+				fmt.Printf("You win. Word is '%s'\n", hiddenWord[numberRand])
+				break
+			}
 		} else {
-			fmt.Println("NOPE! Try again")
+			if iterationGameLose != 5 {
+				fmt.Printf("NO!, there is not '%s'\n", letter)
+				iterationGameLose++
+				attemps--
+				gallowsPrint(attemps)
+			} else {
+				fmt.Printf("You lose. Word was '%s'\n", hiddenWord[numberRand])
+				break
+			}
 		}
 	}
 }
--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
-	var word string
 	words := []string{"golang", "java", "python", "ruby", "javascript"}
+	wordsExplanation := []string{"a compiled multi-threaded programming language developed internally by Google",
+		"a strongly typed, object-oriented, general-purpose programming language developed by Sun Microsystems",
+		"high-level general-purpose programming language with dynamic strong typing and automatic memory management",
+		"dynamic, reflective, interpreted high-level programming language",
+		"multi-paradigm programming language. Supports object-oriented, imperative and functional styles"}
 	min := 0
 	max := len(words) - 1
 	randWordNumber := rand.Intn(max-min) + min
 	l.PrintRules()
 	letterArray := l.TrimWord(words, randWordNumber)
-	l.PrintWordByStep(words, letterArray, word, randWordNumber)
+	l.PrintWordByStep(words, wordsExplanation, letterArray, randWordNumber)
 }
